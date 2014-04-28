be50849e9ffca9a70eb1d839469cb102e6a0fce1
Thibault Pierre <tib.pierre@gmail.com>
2014-04-28 11:20:18 +0200

    Examples: Update bootstrap and add fadeIn

--- examples/js/main.js
+++ examples/js/main.js
@@ -3,17 +3,21 @@ angular.module('myApp', ['lazymage'])
     lazymageRemoteProvider.globalOptions = {
       currentImage: {
         attrs: {
-          class: 'img-responsive'
+          class: 'img-responsive fadein'
         }
       },
       errorImage: {
-        src: 'img/error.jpg'
-      },
-      defaultImage: {
-        src: 'img/loading.jpg',
+        src: 'img/error.jpg',
         attrs:{
           class: 'img-responsive'
         }
+      },
+      defaultImage: {
+        src: 'img/loading.jpg',
+        attrs: {
+          class: 'img-responsive default-img'
+        },
+        removeTimeout: false
       },
       loader: false
     };
@@ -22,18 +26,16 @@ angular.module('myApp', ['lazymage'])
   $scope.options = {
     defaultImage: false,
     loader: {
-      html: '<span class="loader"></span>'
+      html: '<div class="loader"></div>'
     }
   };
 
   // Images from wallbase.cc
   $scope.images = [
-    'http://i.imgur.com/i5xjTTb.jpg',
-    'http://i.imgur.com/vcIWxsL.jpg',
-    'http://i.imgur.com/P0OPc2C.jpg',
-    'http://i.imgur.com/zFDyPOO.jpg',
-    'http://i.imgur.com/GCY2ENZ.jpg',
-    'http://i.imgur.com/CkGkmc0.jpg',
-    'http://i.imgur.com/H3bPge9.jpg'
+    'http://i.imgur.com/B3E2nkP.jpg',
+    'http://i.imgur.com/YBkNRs1.jpg',
+    'http://i.imgur.com/hBhOWDL.jpg',
+    'http://i.imgur.com/2vFmPpE.jpg',
+    'http://i.imgur.com/zfu31ZO.jpg'
   ];
 });
